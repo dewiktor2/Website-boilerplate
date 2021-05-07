7564acee8851342a649f308ffc85d219938723ea
Wiktor Debski <dewiktor@gmail.com>
2021-05-07 18:33:52 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
+    window.addEventListener('load', () => {
         navigator.serviceWorker
             .register('./service-worker.js')
         // eslint-disable-next-line no-console
