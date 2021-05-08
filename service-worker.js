ab7faa9e2c0dfdd8efbe7a1e3df31307d3f31e3d
Wiktor Debski <dewiktor@gmail.com>
2021-05-09 01:23:05 +0200

    Empty service worker

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,1 @@
-const staticDev = 'website-boilerplate';
-const assets = ['images/icons'];
-
-self.addEventListener('install', (installEvent) => {
-    installEvent.waitUntil(
-        caches.open(staticDev).then((cache) => {
-            cache.addAll(assets);
-        })
-    );
-});
-
-self.addEventListener('fetch', (fetchEvent) => {
-    fetchEvent.respondWith(
-        caches.match(fetchEvent.request).then((res) => {
-            return res && fetch(fetchEvent.request);
-        })
-    );
-});
+self.addEventListener('install', () => {});
